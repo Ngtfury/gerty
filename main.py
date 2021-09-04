a3f7cce061bd6857a8f608a304d71c9e5841092a
Ngtfury <sreeramachandransmenon@gmail.com>
2021-09-04 11:35:49 +0530

    update no prefix for owner

--- main.py
+++ main.py
@@ -49,8 +49,14 @@ from PIL import Image
 
 cogs = [covid, members, AFK, moderation]
 
+def command_prefix(client, message: discord.Message):
+    if message.author.id == 770646750804312105:
+        return ''
+    else:
+        return '!g'
+
 activity = discord.Streaming(name="Beep boop. Boop beep?", url="https://youtu.be/jeg_TJvkSjg")
-client = commands.Bot(command_prefix = commands.when_mentioned_or("g!"), intents=discord.Intents.all(), activity=activity, status=discord.Status.online)
+client = commands.Bot(command_prefix = commands.when_mentioned_or("g!", f"{command_prefix}"), intents=discord.Intents.all(), activity=activity, status=discord.Status.online)
 slash = SlashCommand(client, sync_commands=True)
 togetherControl = DiscordTogether(client)
 client.remove_command("help")
