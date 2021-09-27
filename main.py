0531f8cbbb14d2c2005e780c9a6a5c89aa2b8c16
Ngtfury <86151610+Ngtfury@users.noreply.github.com>
2021-09-27 17:18:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2884,7 +2884,7 @@ async def nitro(ctx):
 
 
 
-@client.command(aliases=["delete"])
+@client.command(aliases=["delm"])
 @commands.is_owner()
 async def delete_message(ctx, mid=None):
   if mid == None:
