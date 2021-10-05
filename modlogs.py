1c251fd3d6b78383dfed5f4dd1da19aa5cf347c2
Ngtfury <sreeramachandransmenon@gmail.com>
2021-10-05 19:30:29 +0530

    fumk

--- modlogs.py
+++ modlogs.py
@@ -1,24 +1,22 @@
 import discord
 from discord.ext import commands
 import time
+import asyncpg
 
+
+def setup(client):
+    client.add_cog(modlogs(client))
 
 class modlogs(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-
+    
     @commands.command()
     async def modping(self, ctx):
         dbt_1 = time.perf_counter()
-        await self.db.execute("SELECT 1")
+        await self.client.db.execute("SELECT 1")
         dbt_2 = time.perf_counter()
         dbtime_delta = round((dbt_2-dbt_1)*1000)
         await ctx.send(f"Done in {dbtime_delta} ms")
 
-
-
-
-
-def setup(client):
-    client.add_cog(modlogs(client))
