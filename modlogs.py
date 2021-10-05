f65e66a46ca5a5fc388334076ac214862bd36834
Ngtfury <sreeramachandransmenon@gmail.com>
2021-10-05 20:07:18 +0530

    idk idk idk

--- modlogs.py
+++ modlogs.py
@@ -27,13 +27,44 @@ class modlogs(commands.Cog):
 
     @modlog.command()
     async def channel(self, ctx, channel: discord.TextChannel):
+        try:
+            g = await channel.send(".")
+            await g.delete()
+        except:
+            await ctx.send(f"I cannot send message to {channel.mention}")
+        else:
+            result = await self.client.db.fetchrow("SELECT channel_id FROM mod_logs WHERE guild_id = $1", ctx.guild.id)
+            if not result:
+                await self.client.db.execute("INSERT INTO mod_logs (guild_id, channel_id) VALUES ($1,$2)", ctx.guild.id, channel.id)
+                em = discord.Embed(description=f"<:success:893501515107557466> Mod logs channel **set** to {channel.mention}", color=0x2F3136)
+                await ctx.send(embed=em)
+            else:
+                await self.client.db.execute("UPDATE mod_logs SET channel_id = $1 WHERE guild_id = $2", channel.id, ctx.guild.id)
+                em = discord.Embed(description=f"<:success:893501515107557466> Mod logs channel **updated** to {channel.mention}", color=0x2F3136)
+                await ctx.send(embed=em)
+    
+    @modlog.command()
+    async def delete(self, ctx):
         result = await self.client.db.fetchrow("SELECT channel_id FROM mod_logs WHERE guild_id = $1", ctx.guild.id)
-        if not result:
-            await self.client.db.execute("INSERT INTO mod_logs (guild_id, channel_id) VALUES ($1,$2)", ctx.guild.id, channel.id)
-            em = discord.Embed(description=f"<:success:893501515107557466> Mod logs channel **set** to {channel.mention}", color=0x2F3136)
+        if result:
+            await self.db.execute("DELETE FROM mod_logs WHERE guild_id = $1", ctx.guild.id)
+            em = discord.Embed(description=f"<:success:893501515107557466> Successfully **deleted** mod logs data for this server", color=0x2F3136)
             await ctx.send(embed=em)
         else:
-            await self.client.db.execute("UPDATE mod_logs SET channel_id = $1 WHERE guild_id = $2", channel.id, ctx.guild.id)
-            em = discord.Embed(description=f"<:success:893501515107557466> Mod logs channel **updated** to {channel.mention}", color=0x2F3136)
+            em = discord.Embed(description=f"<:success:893501515107557466> There is no mod logs channel for this server in my database to delete", color=0x2F3136)
             await ctx.send(embed=em)
+
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        result = await self.client.db.fetchrow("SELECT channel_id FROM mod_logs WHERE guild_id = $1", message.guild.id)
+        if result:
+            em = discord.Embed(color=0x2F3136)
+            em.set_author(name=f"{message.author}", icon_url=f"{message.author.avatar_url}")
+            em.add_field(name=f"{message.author.name} deleted a message in {message.channel.mention}", value=f"{message.content}")
+
+            channel = self.client.get_channel(result[0])
+            await channel.send(embed=em)
+
+        
 
