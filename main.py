de07f10f634ae02c1754963c9d2b1a24ca9231f0
Ngtfury <sreeramachandransmenon@gmail.com>
2021-11-01 23:20:09 +0530

    this will work?

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import urllib.request
 import urllib.parse
 import hashlib
 import requests
+import re
 import aiofiles
 import datetime
 import asyncio
@@ -66,7 +67,16 @@ client.db = client.loop.run_until_complete(asyncpg.create_pool(host="ec2-54-162-
 
 
 
+def source(o):
+  s = inspect.getsource(o).split("\n")
+  indent = len(s[0]) - len(s[0].lstrip())
+  return "\n".join(i[indent:] for i in s)
 
+source_=source(discord.gateway.DiscordWebSocket.identify)
+patched=re.sub(r'([\'"]\$browser[\'"]:\s?[\'"]).+([\'"])', r"\1Discord Android\2", source_)
+loc = {}
+exec(compile(ast.parse(patched), "<string>", "exec"),discord.gateway.__dict__, loc)
+discord.gateway.DiscordWebSocket.identify = loc["identify"]
 
 
 
