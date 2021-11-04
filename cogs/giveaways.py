56fb71649162f7f0771bdaf1a1391c49ca70446f
Ngtfury <sreeramachandransmenon@gmail.com>
2021-11-04 22:50:20 +0530

    BROOOOOOO

--- cogs/giveaways.py
+++ cogs/giveaways.py
@@ -48,7 +48,7 @@ class Giveaways(commands.Cog):
         main_message=await ctx.send(embed=firstembed)
         await asyncio.sleep(3)
         
-        em=discord.Embed(description=f'<:prize:905859038317776926> **Prize**: {prize}\n<a:timer:905859476257656872> Timer: <t:{timenow+time}:R>\n<:winner:905859555852967946> Host: {ctx.author.mention}\n\nReact with 🎉 to participate!\nTo end the giveaway, type:\ng!end {main_message.id}', color=0x2F3136)
+        em=discord.Embed(description=f'<:prize:905859038317776926> **Prize**: {prize}\n<a:timer:905859476257656872> Timer: <t:{timenow+time}:R>\n<:winner:905859555852967946> Host: {ctx.author.mention}\n\nReact with 🎉 to participate!\nTo end the giveaway, type:\n`g!end {main_message.id}`', color=0x2F3136)
         em.set_author(name=f'{ctx.channel.name} Giveaways!', icon_url=ctx.guild.icon_url)
         em.set_image(url='https://i.imgur.com/USGQsyz.png')
         await main_message.edit('🎉 **GIVEAWAY** 🎉', embed=em)
@@ -57,7 +57,7 @@ class Giveaways(commands.Cog):
         await asyncio.sleep(time)
         ok_message=await ctx.channel.fetch_message(main_message.id)
         win=[]
-        entries=ok_message.reactions[0]
+        entries=ok_message.reactions
         for x in entries:
             if not x.user==self.bot.user:
                 win.append(x)
