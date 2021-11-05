4f9cf93b54b0e9f1d2d42f39e5a3808c52ec8728
Ngtfury <sreeramachandransmenon@gmail.com>
2021-11-05 20:41:54 +0530

    bruda

--- main.py
+++ main.py
@@ -1163,50 +1163,7 @@ buttons = [
     ],
 ]
  
-#calculates answer
-def calculate(exp):
-    o = exp.replace('×', '*')
-    o = o.replace('÷', '/')
-    result = ''
-    try:
-        result = str(eval(o))
-    except:
-        result = 'An error occurred.'
-    return result
  
-@client.command(brief='meta', usage='calc', description='Sends an interactive calculator')
-async def calc(ctx):
-    em = discord.Embed(description="> <a:gloading:855680101529944064> Loading calculator..")
-    m = await ctx.send(embed=em)
-    expression = 'None'
-    delta = datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
-    e = discord.Embed(title=f'{ctx.author.name}\'s calculator | {ctx.author.id}', description=expression,
-                        timestamp=delta)
-    await m.edit(components=buttons, embed=e)
-    while m.created_at < delta:
-        res = await client.wait_for('button_click')
-        if res.author.id == int(res.message.embeds[0].title.split('|')[1]) and res.message.embeds[
-            0].timestamp < delta:
-            expression = res.message.embeds[0].description
-            if expression == 'None' or expression == 'An error occurred.':
-                expression = ''
-            if res.component.label == 'Exit':
-                await res.respond(content='Calculator Closed', type=7)
-                await asyncio.sleep(2)
-                await m.delete()
-                break
-            elif res.component.label == '←':
-                expression = expression[:-1]
-            elif res.component.label == 'Clear':
-                expression = 'None'
-            elif res.component.label == '=':
-                expression = calculate(expression)
-            else:
-                expression += res.component.label
-            f = discord.Embed(title=f'{res.author.name}\'s calculator|{res.author.id}', description=expression,
-                                timestamp=delta)
-            await res.respond(content='', embed=f, components=buttons, type=7)
-  
 
 @client.command(brief='fun', usage='grayscale [user]', description='Grayscales a users avatar')
 async def grayscale(ctx, user: discord.Member=None):
