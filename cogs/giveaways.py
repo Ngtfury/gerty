3fb9037c1220fe5e466871fcf1318b2a9c57d1c1
Ngtfury <sreeramachandransmenon@gmail.com>
2021-11-05 19:46:10 +0530

    ok

--- cogs/giveaways.py
+++ cogs/giveaways.py
@@ -36,10 +36,6 @@ class Giveaways(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_channels=True)
     async def gstart(self, ctx, time, *, prize):
-        try:
-            await ctx.message.delete()
-        except:
-            pass
         timenow=int(datetime.datetime.now().timestamp())
 
         time=self.convert(time)
@@ -47,6 +43,11 @@ class Giveaways(commands.Cog):
             return await ctx.send("You didn't answer the proper unit. Use (s|m|h|d) next time!")
         elif time==-2:
             return await ctx.send('The time must be an integer. Please enter an integer next time')
+
+        try:
+            await ctx.message.delete()
+        except:
+            pass
 
         firstembed=discord.Embed(description='<a:timer:905859476257656872> **Loading giveaway** <a:timer:905859476257656872>', color=0x2F3136)
         main_message=await ctx.send(embed=firstembed)
@@ -70,3 +71,21 @@ class Giveaways(commands.Cog):
 
 
         await main_message.edit('🎉 **GIVEAWAY ENDED** 🎉')
+
+
+    @commands.command()
+    async def greroll(self, ctx, message_id:discord.Message):
+        if message_id.author != self.bot.user:
+            return await ctx.send('That is not a message sent by me!')
+
+        if not message_id.content.startswith('🎉 **GIVEAWAY ENDED** 🎉'):
+            return await ctx.send('This is not a giveaway message or this giveaway must end first to reroll')
+
+        message=await message_id.channel.fetch_message(message_id.id)
+        entries=await message.reactions[0].users().flatten()
+        entries.pop(entries.index(self.bot.user))
+
+        winner=random.choice(entries)
+
+        lastembed=discord.Embed(description=f'**🎉 [Link to giveaway]({message.jump_url}) | [Invite me!]({discord.utils.oauth_url(self.bot.user.id)})**', color=0x2F3136)
+        await ctx.send(f'Congratulations the new winner is 🎉 {winner.mention}!', embed=lastembed)
