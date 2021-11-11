28c488ffc9f8da10543f371f85742cb673945661
Ngtfury <sreeramachandransmenon@gmail.com>
2021-11-11 23:45:14 +0530

    ok

--- main.py
+++ main.py
@@ -1475,7 +1475,7 @@ async def nitro(ctx):
 
   while True:
     try:
-      event = await client.wait_for('button_click', check=lambda i: i.channel==ctx.channel and i.message==main, timeout=10) #
+      event = await client.wait_for('button_click', check=lambda i: i.channel==ctx.channel and i.message==main, timeout=40) #
       if event.author==ctx.author:
         await event.respond(type=4, content="You cannot claim your gift yourself!")
       elif event.component.id=='NitroButton':
