6a01cbb94f44711b281ba13e5bc1e05d65ec8aa9
Ngtfury <sreeramachandransmenon@gmail.com>
2021-11-17 23:03:01 +0530

    ohk

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ async def on_command_error(ctx, error):
     _oh=[f'`{x}`' for x in error.missing_perms]
     oh_=', '.join(_oh)
     _perms=oh_.replace('_', " ")
-    em = discord.Embed(description=f"<:error:893501396161290320> You are missing following permissions to run this command, {_perms}", color=0x2F3136)
+    em = discord.Embed(description=f"<:error:893501396161290320> You are missing {_perms} permission(s) to run this command", color=0x2F3136)
     await ctx.reply(embed=em, mention_author=False)
   elif isinstance(error, commands.NotOwner):
     em = discord.Embed(description="<a:zpanda_heart:907292207604723743> This is an owner-only command and you don't look like `Nιgнт Fυяу ♪🤍#4371`", color=0x2F3136)
