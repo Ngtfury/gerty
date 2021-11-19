debf146e679058c98bda0a8e6fb505aa2cb99bfb
Ngtfury <sreeramachandransmenon@gmail.com>
2021-11-19 20:11:02 +0530

    ok

--- cogs/image.py
+++ cogs/image.py
@@ -26,7 +26,7 @@ class ImageCommands(commands.Cog):
 
 
     @commands.command(brief='image', usage='[user or custom emoji or url]', description='Changes an emoji image or user avatar or an image to emoji form')
-    async def emojify(self, ctx, object: typing.Union[discord.User, discord.PartialEmoji, discord.Message, str]=None):
+    async def emojify(self, ctx, object: typing.Union[discord.Member, discord.PartialEmoji, discord.Message, str]=None):
         await ctx.trigger_typing()
         if object==None:
             if ctx.message.reference:
@@ -35,7 +35,7 @@ class ImageCommands(commands.Cog):
                 object=ctx.author
         if isinstance(object, discord.PartialEmoji):
             _url=str(object.url)
-        elif isinstance(object, discord.User):
+        elif isinstance(object, discord.Member):
             _url=str(object.avatar_url)
         elif isinstance(object, discord.Message):
             _urllist=re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', object.content)
@@ -75,7 +75,7 @@ class ImageCommands(commands.Cog):
 
     
     @commands.command(brief='image', usage='[user or custom emoji or url]', description='Adds some heart effects to the image')
-    async def hearts(self,ctx, object: typing.Union[discord.User, discord.PartialEmoji, discord.Message, str]=None):
+    async def hearts(self,ctx, object: typing.Union[discord.Member, discord.PartialEmoji, discord.Message, str]=None):
         await ctx.trigger_typing()
         time1=time.perf_counter()
         if object==None:
@@ -86,7 +86,7 @@ class ImageCommands(commands.Cog):
 
         if isinstance(object, discord.PartialEmoji):
             _url=str(object.url)
-        elif isinstance(object, discord.User):
+        elif isinstance(object, discord.Member):
             _url=str(object.avatar_url)
         elif isinstance(object, discord.Message):
             _urllist=re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', object.content)
@@ -122,7 +122,7 @@ class ImageCommands(commands.Cog):
 
 
     @commands.command(brief='image', usage='[user or custom emoji or url]', description='Creates a maths equation meme with an image', aliases=['math'])
-    async def equation(self, ctx, object: typing.Union[discord.User, discord.PartialEmoji, discord.Message, str]=None):
+    async def equation(self, ctx, object: typing.Union[discord.Member, discord.PartialEmoji, discord.Message, str]=None):
         await ctx.trigger_typing()
         time1=time.perf_counter()
         if object==None:
@@ -133,7 +133,7 @@ class ImageCommands(commands.Cog):
 
         if isinstance(object, discord.PartialEmoji):
             _url=str(object.url)
-        elif isinstance(object, discord.User):
+        elif isinstance(object, discord.Member):
             _url=str(object.avatar_url)
         elif isinstance(object, discord.Message):
             _urllist=re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', object.content)
@@ -169,7 +169,7 @@ class ImageCommands(commands.Cog):
 
 
     @commands.command(brief='image', description='Gives boil effect to an image', usage='[user or custom emoji or url]')
-    async def boil(self, ctx, object: typing.Union[discord.User, discord.PartialEmoji, discord.Message, str]=None):
+    async def boil(self, ctx, object: typing.Union[discord.Member, discord.PartialEmoji, discord.Message, str]=None):
         await ctx.trigger_typing()
         time1=time.perf_counter()
         if object==None:
@@ -180,7 +180,7 @@ class ImageCommands(commands.Cog):
 
         if isinstance(object, discord.PartialEmoji):
             _url=str(object.url)
-        elif isinstance(object, discord.User):
+        elif isinstance(object, discord.Member):
             _url=str(object.avatar_url)
         elif isinstance(object, discord.Message):
             _urllist=re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', object.content)
@@ -254,6 +254,54 @@ class ImageCommands(commands.Cog):
  
         async with aiohttp.ClientSession() as session:
             async with session.get(f'https://api.jeyy.xyz/image/sob?image_url={_url}') as r:
+                if r.status != 200:
+                    return await ctx.send(embed=Utils.BotEmbed.error(f'Sorry there was an error, {r.reason}'))
+                buf=BytesIO(await r.read())
+        time2=time.perf_counter()
+        timedelta=time2-time1
+        await ctx.reply(f'Process took `{timedelta}` seconds', file=discord.File(buf, 'boiled.gif'), mention_author=False)
+
+
+
+    @commands.command(brief='image', description='Rotates an image (gif)', usage='[user or custom emoji or url]', aliases=['rotate'])
+    async def roll(self, ctx, object: typing.Union[discord.Member, discord.PartialEmoji, discord.Message, str]=None):
+        await ctx.trigger_typing()
+        time1=time.perf_counter()
+        if object==None:
+            if ctx.message.reference:
+                object=ctx.message.reference.resolved
+            else:
+                object=ctx.author
+
+        if isinstance(object, discord.PartialEmoji):
+            _url=str(object.url)
+        elif isinstance(object, discord.Member):
+            _url=str(object.avatar_url)
+        elif isinstance(object, discord.Message):
+            _urllist=re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', object.content)
+            if _urllist:
+                _url=''.join(_urllist)
+            else:
+                _urllist=re.findall(r'<(?P<animated>a?):(?P<name>[a-zA-Z0-9_]{2,32}):(?P<id>[0-9]{18,22})>', object.content)
+                if _urllist:
+                    _urllisttuple=_urllist[0]
+                    EmojiId=_urllisttuple[2]
+                    _emoji=self.bot.get_emoji(int(EmojiId))
+                    _url=str(_emoji.url)
+                else:
+                    return await self.api_error(ctx)
+        elif isinstance(object, str):
+            _urllist=re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(object))
+            if not _urllist:
+                return await self.api_error(ctx)
+            else:
+                _url=str(object)
+        else:
+            return await self.api_error(ctx)
+
+ 
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://api.jeyy.xyz/image/roll?image_url={_url}') as r:
                 if r.status != 200:
                     return await ctx.send(embed=Utils.BotEmbed.error(f'Sorry there was an error, {r.reason}'))
                 buf=BytesIO(await r.read())
