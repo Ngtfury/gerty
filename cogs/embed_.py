0b8ea25e9582a780187a38800fc4c07429bf01c2
Ngtfury <sreeramachandransmenon@gmail.com>
2021-11-30 15:14:53 +0530

    breh

--- cogs/embed_.py
+++ cogs/embed_.py
@@ -146,6 +146,9 @@ class EmbedEditor(commands.Cog):
                         if str(e) in ['Invalid Form Body', 'Not a well formed URL.']:
                             await event.respond(type=4, content='Not a well formed image URL provided in footer icon.')
                             continue
+                        else:
+                            await event.respond(type=4, content=f'{str(e)}')
+                            continue
 
             except asyncio.TimeoutError:
                 try:
