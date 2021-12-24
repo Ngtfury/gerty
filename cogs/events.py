837883af9d9a34dc6fa4d8eeb139d87d2c70ec0b
Ngtfury <sreeramachandransmenon@gmail.com>
2021-12-24 21:31:49 +0530

    fix

--- cogs/events.py
+++ cogs/events.py
@@ -163,7 +163,7 @@ class events(commands.Cog):
             if matches:
                 view = ErrorMatchesView(ctx)
                 view.add_item(ErrorMatchExecute(ctx, matches[0]))
-                await ctx.send(
+                view.message = await ctx.send(
                     f"""Sorry, but the command **{ctx.invoked_with}** was not found
 did you mean **`{matches[0]}`**?""",
                     view = view
